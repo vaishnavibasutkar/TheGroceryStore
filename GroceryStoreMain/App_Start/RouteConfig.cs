0bf52cee1d48ab8e75fd8099db2108ac18c24573
Acer <vaishnavibasutkar2@gmail.com>
2022-02-13 00:55:11 +0530

    Add project files.

---
+++ GroceryStoreMain/App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace GroceryStoreMain
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Admin",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Distributor",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Distributor", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
